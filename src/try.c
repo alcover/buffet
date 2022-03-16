e456ea06c67d1e2f3fc6b05224faaa05fb59d46e
alcover <francois@alcover.fr>
2022-03-16 13:09:26 +0100

    trigger alias freeing

--- src/try.c
+++ src/try.c
@@ -18,17 +18,24 @@ Buffet pass_owner(){
 
 int main() {
 
-	Buffet own = pass_owner();
-	Buffet ref = buffet_view (&own, 10, 4); // "abcd"
-	buffet_free(&own);
-	buffet_free(&ref);
+	// Buffet own = pass_owner();
+	// Buffet ref = buffet_view (&own, 10, 4); // "abcd"
+	// buffet_free(&own);
+	// buffet_free(&ref);
 
-{
-	Buffet src;
-	buffet_strcopy(&src, "Bonjour", 7);
-	Buffet ref = buffet_view(&src, 0, 3);
-	buffet_dbg(&ref);
-	buffet_print(&ref);
+{	// aliasing then free
+	Buffet own;
+	buffet_strcopy(&own, alpha, 40);
+	Buffet  cpy_alias = own;
+	Buffet *ptr_alias = &own;
+
+	buffet_free(&own);
+
+	buffet_free(&cpy_alias);
+	buffet_dbg (&cpy_alias);
+
+	buffet_free(ptr_alias);
+	buffet_dbg (ptr_alias);
 }
 
 	return 0;
